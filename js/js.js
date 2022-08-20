3557d489064004852f6f566735ed0302b85beb52
karim-eprahim <karimalhawy0@gmail.com>
2022-08-19 19:16:11 -0700

    new

---
+++ js/js.js
@@ -0,0 +1,49 @@
+let ptn =document.querySelector(".menu-ptn")
+let nav =document.querySelector(".navbar")
+let round =document.querySelectorAll('.round a')
+let bage =document.querySelector(".bage")
+let menu =document.querySelectorAll(".menu-ptn span")
+
+let scrol =document.querySelector(".scroll")
+/*********** */
+let skills =document.querySelector(".insights")
+let circls = document.querySelectorAll(".progress svg circle")
+//************* */
+
+
+// console.log(menu)
+
+ptn.onclick=function(){
+    nav.classList.toggle("open")
+    ptn.classList.toggle("close")
+
+}
+for(let i=0; i<round.length;i++){
+    round[i].onclick=()=>{
+        nav.classList.toggle("open")
+    }
+}
+
+window.onscroll=()=>{
+    if(window.scrollY>=30){
+        scrol.style.cssText=`visibility:visible; z-index: 1;`
+    }else{
+        scrol.style.cssText=`visibility:hidden;`
+    }
+
+    // skills animation 
+
+    if(window.scrollY >= skills.offsetTop -420){
+
+        circls.forEach(span => {
+            span.style.strokeDashoffset = span.dataset.dashoffset;
+        });
+    }
+}
+scrol.onclick=()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+
+}
